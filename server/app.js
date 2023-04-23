9adc6fb7384333213d86c15c5c00568d86a4ae29
daniel.yoshizawa <yoshidanielcwb@gmail.com>
2023-04-23 13:32:50 -0300

    Adding routes to the server

--- server/app.js
+++ server/app.js
@@ -1,9 +1,18 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 const app = express()
 const port = 3001
 
+app.set('port', port)
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
   res.send('Hello World Server')
+})
+
+app.post('/', (req, res) => {
+  res.write(JSON.stringify(req.body, null, 2))
+  res.end()
 })
 
 app.listen(port, () => {
